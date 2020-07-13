89f108b36b8006dcf281df01806b1ec5d09c4f80
MajemiteJames <majemiteokoro@gmail.com>
2020-07-13 18:02:02 +0100

    implemented the function to get movies based on netflix original and trending to show on the browser

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import Row from './Row';
+import request from './request';
 
 function App() {
   return (
     <div className="App">
       <h1>Building Netflix Clone Front-end !</h1>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals}/>
+      <Row title="TRENDING NOW" fetchUrl={request.fetchTrending}/>
     </div>
   );
 }
