580dc7e679648ce8d69aac8102d88beb5af752c1
MajemiteJames <majemiteokoro@gmail.com>
2020-07-14 01:09:36 +0100

    implemented the getting of all movies and represnetation of all the rows

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import request from './request';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Building Netflix Clone Front-end !</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals}/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={request.fetchNetflixOriginals} isLargeRow/>
       <Row title="TRENDING NOW" fetchUrl={request.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={request.fetchTopRated}/>
+      <Row title="Action Movies" fetchUrl={request.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={request.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={request.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={request.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={request.fetchDocumentaries}/>
     </div>
   );
 }
